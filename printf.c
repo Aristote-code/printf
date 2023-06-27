6e30808f9569a0f55dbae571ff43c7a00f44caa0
Okyere Adubofuor <oksberg89@gmail.com>
2023-06-27 02:35:22 -0700

    Getting to the printf

--- printf.c
+++ printf.c
@@ -1,28 +1,13 @@
 /*
  * File: _printf.c
  * Auth: Gahima Aristote
- *			
  */
-
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include <limits.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);
-int printchar(char s);
 int _strlen(const char *str);
-
-/**
- * printchar - Writes the character s to stdout
- * @s: Character to write
- *
- * Return: 1 on success, -1 on error.
- */
-int printchar(char s)
-{
-return (write(1, &s, 1));
-}
 
 /**
  * _strlen - Counts the length of a string
@@ -101,58 +86,4 @@ count++;
 
 va_end(args);
 return (count);
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-int len;
-int len2;
-unsigned int ui;
-void *addr;
-
-len = _printf("Let's try to printf a simple sentence.\n");
-len2 = printf("Let's try to printf a simple sentence.\n");
-ui = (unsigned int)INT_MAX + 1024;
-addr = (void *)0x7ffe637541f0;
-
-_printf("Length:[%d, %i]\n", len, len);
-printf("Length:[%d, %i]\n", len2, len2);
-
-_printf("Negative:[%d]\n", -762534);
-printf("Negative:[%d]\n", -762534);
-
-_printf("Unsigned:[%u]\n", ui);
-printf("Unsigned:[%u]\n", ui);
-
-_printf("Unsigned octal:[%o]\n", ui);
-printf("Unsigned octal:[%o]\n", ui);
-
-_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-
-_printf("Character:[%c]\n", 'H');
-printf("Character:[%c]\n", 'H');
-
-_printf("String:[%s]\n", "I am a string !");
-printf("String:[%s]\n", "I am a string !");
-
-_printf("Address:[%p]\n", addr);
-printf("Address:[%p]\n", addr);
-
-len = _printf("Percent:[%%]\n");
-len2 = printf("Percent:[%%]\n");
-
-_printf("Len:[%d]\n", len);
-printf("Len:[%d]\n", len2);
-
-_printf("Unknown:[%c]\n", 'r');
-printf("Unknown:[%c]\n", 'r');
-
-return (0);
 }
